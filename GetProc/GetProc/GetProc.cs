97c5dfd7effa604bead3ac9338e2c45c255c9fbe
Arslan Ahmad <ars.ahm6@gmail.com>
2018-12-13 12:49:01 +0500

    dsfdsfds

---
+++ GetProc/GetProc/GetProc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace GetProc
+{
+    [Cmdlet(VerbsCommon.Get, "Proc")]
+    public class GetProc : Cmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            WriteDebug("Begin processing");
+        }
+
+        protected override void EndProcessing()
+        {
+            WriteDebug("End processing");
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteDebug("Process record");
+            WriteObject(System.Diagnostics.Process.GetProcesses(), true);
+        }
+
+        protected override void StopProcessing()
+        {
+            WriteDebug("Stopp processing");
+        }
+    }
+}
---
+++ GetProc/GetProc/GetProcSnapIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.ComponentModel;
+
+namespace GetProc
+{
+    [RunInstaller(true)]
+    public class GetProcSnapIn : PSSnapIn
+    {
+        public override string Name => "GetProcSnapin";
+
+        public override string Vendor => "RAVIAN656";
+
+        public override string Description => "RAVIAN656.GetProcSnapIn";
+    }
+}
